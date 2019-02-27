64d6beb0dd8e0734880bd920a94fcc2556364519
Penguin825 <aric.fowler@gmail.com>
2019-02-27 19:59:19 +0000

    started on shit

--- run.py
+++ run.py
@@ -1,27 +1,69 @@
-import test, formativeTest, summativeTest
-import student, lecturer
+import test, formativeTest, summativeTest, student, lecturer
+import os
 from tkinter import *
 
 class program:
     #constructors
-    def __init__(self, master):
-        self.master = master
-        master.title("QuizMachine v0.1")
+    def __init__(self, root):
+        #create startup page
+        self.root = root
+        root.title("Log in")
+        root.geometry("500x300")
 
-        self.label = Label(master, text="Welcome to the Quiz machine!\nPlease log in:")
+        #page contents
+        self.label = Label(root, text="\nWelcome to the Quiz machine!\nPlease log in:\n")
         self.label.pack()
 
-        self.startButton = Button(master, text="Student", command=self.startStudent)
-        self.startButton.pack()
+        self.stuButton = Button(root, text="Student", command=self.startStudent)
+        self.stuButton.pack()
 
-        self.closeButton = Button(master, text="Lecturer", command=self.startLecturer)
-        self.closeButton.pack(side="left")
+        self.lecButton = Button(root, text="Lecturer", command=self.startLecturer)
+        self.lecButton.pack()
+
 
     #methods
     def startStudent(self):
+        #create new window
+        winStudent = Toplevel(self.root)
+        winStudent.title("Logged in as Student")
+        winStudent.geometry("500x300")
+
+        #page contents
+        self.label = Label(winStudent, text="\nStudent portal\n")
+        self.label.pack()
+
+        self.takeButton = Button(winStudent, text="Take a test", command=lambda: self.studentTest(winStudent))
+        self.takeButton.pack()
+
+        self.feedButton = Button(winStudent, text="Look at feedback", command=self.studentFeedback)
+        self.feedButton.pack()
+        
+        #close startup page
+        self.root.withdraw()
+        return
+
+    def studentTest(self, winStudent):
+        self.feedButton.destroy()
+        
+        self.label2 = Label(winStudent, text="\nChoose a test:\n")
+        self.label2.pack()
+
+        tests = [filename for filename in os.listdir("/tests") if os.path.splitext(filename)[1] == ".txt"]
+        
+        self.testMenu = OptionMenu(winStudent, "", *tests)
+        self.testMenu.pack()
+        return
+
+    def studentFeedback(self):
         return
 
     def startLecturer(self):
+        #create new window
+        winLecturer = Toplevel(self.root)
+        winLecturer.title("Logged in as Lecturer")
+        winLecturer.geometry("500x300")
+        #close startup page
+        self.root.withdraw()
         return
 
 
